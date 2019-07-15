23b256bd2964335ad8dc0dc761f5286ec5af59d6
root <root@pop-os.localdomain>
2019-07-14 22:52:29 -0400

    more work done, but weird error please send help

--- ARTE/src/main.rs
+++ ARTE/src/main.rs
@@ -1,8 +1,5 @@
-//File: main.rs
-//Created by Andy Mahoney
-//on: 5/9/19
-//last updated: 5/12/19
-//contact: andym AT fortrash DOT com
+//file created by Andy Mahoney
+//last edited on 6/16/19
 
 use std::env;
 use std::fs::File;
@@ -18,94 +15,42 @@ use std::fs::read_to_string;
 use std::io::{BufReader, BufWriter};
 use ropey::Rope;
 
-struct gen_info{
-    splitTotal: usize, //tells other functions how many splits currently exist
-    tabTotal: i32, //tells other functions how many splits currently exist
-    desktopTotal: i32, //tells other functions how many splits currently exist
-    currentSplit: i32, //current split
-    currentTab: i32, //curent tab
-    currentDesktop: i32, // current desktop
-    screenheight: i64,
-    topLine: i64, //top line displayed
-    bottomLine: i64, //bottom line displayes
-}
+pub static displayHeight: usize = 50;
 
-pub struct split{ //a split is what we are calling a window with in a tab, there can be 1 split to tab or n splits in a tab
-        content: String, //a vector of ropes where each rope is a lone from the file
-        currentLine: (String, i64), //what the current line is (content of line, line number) 
-        //fileHistory, //stack of file history versions for the sessions
-        filename: String, //filename of file open in split
-        coordinates: (usize,i32,i32), //(what split, what tab, what desktop)
-        topLine: i64, //line number being displayed on top of split
-        bottomLine: i64, //line number being displayed on bottom of split
-}
-
-fn main() {
+fn main(){
     let args: Vec<String> = env::args().collect(); //accepts cli arguements 
     let filename =&args[1]; //takes file name from arg
     let path = Path::new(filename); //saves path name
     let display = path.display(); //Honnestly I have no idea people just always have path followed by this so I figured it wouldn't hurt 
-    let mut GenInfo: gen_info = gen_info{ splitTotal: 0, tabTotal: 0, desktopTotal: 0, currentSplit: 0, currentTab: 0, currentDesktop: 0, screenheight: 50, topLine: 0, bottomLine: 50 };
-    GenInfo.bottomLine = GenInfo.topLine + GenInfo.screenheight;
-    println!("opening file: {}", filename); 
-    let file = OpenOptions::new().read(true).write(true).create(true).open(filename); //sets read and write privalages to true, and creates file if its not present
-    let mut contents = Rope::from_reader(
-        File::open(path).expect("bad path")
-    ).expect("getting contents failed"); 
-    let start_idx = contents.line_to_char(0);
-    let end_idx = contents.line_to_char(4);
-    //.expect("bad read"); //contents now equals the content of the file
-    println!("File = {}", filename);
-    println!("Contents = {}", contents.slice(start_idx..end_idx));
-    println!("contents line 4 {}", contents.line(3));
-    let lineDispMax: i64 = 50;
-    //let mut topLine: i64 =0;
-    //let mut bottomLine: i64 = topLine+lineDispMax;
-    //let screenheight: i64 = 50;
-    //display(contents);
+    let mut totalSplit: i32 =1; //keep track of the number of splits that exist, used as arguements in functions
+    let mut totalTab: i32 =1; //above but tabs
+    let mut totalDesktop: i32 =1; // above but desktops
+    let mut coordinates: (i32, i32, i32)=(1,1,1); //coordinates of split tab and desktop in that order to send as arguements 
+    println!("Welcome to ARTE! \n E: Edit file \n O: open file \n S: new split \n T: new tab \n D: new Desktop \n W: Write to file \n Q: quit \n M: return to main menu "); //basic menu
+    mode_Select();
 }
-/* Under construction fines doubled
-fn display(content){
-    //let mut topLine: i64 =0;
-    //let mut bottomLine: i64 = topLine+screenheight;
-    let mut num = 0;
-    while num != screenheight {
-        println!("{}    ", num+1);
-        println!("{}", content[num]);
-        println!("\n");
-        num-=-1; //lol 
-    }
-}
-Leaving construction area */
-
-unsafe fn open(filename: String){
-    let path = Path::new(&filename); //saves path name
-    let display = path.display(); //Honnestly I have no idea people just always have path followed by this so I figured it wouldn't hurt 
-    let file = OpenOptions::new().read(true).write(true).create(true).open(filename); //sets read and write privalages to true, and creates file if its not present
-    let mut contents: String = fs::read_to_string(path).expect("bad read"); //contents now equals the content of the file
-    let mut content: Vec<String> = Vec::new();
-    let mut GenInfo2: gen_info = gen_info{ 
-        splitTotal: 0, 
-        tabTotal: 0, 
-        desktopTotal: 0, 
-        currentSplit: 0, 
-        currentTab: 0, 
-        currentDesktop: 0, 
-        screenheight: 50, 
-        topLine: 0, 
-        bottomLine: 50, 
+fn mode_Select(){
+    println!("This doesn't exist yet sorta");
+    println!("Mode Select: \n E: Edit file \n O: open file \n S: new split \n T: new tab \n D: new Desktop \n W: Write to file \n Q: quit \n M: return to main menu "); //basic menu
+    let mut menuOption = String::new(); 
+    io::stdin().read_line(&mut menuOption).expect("input error on menuOption"); //gets menu option selected by user
+    match menuOption.as_str(){
+        "E" => edit(&filename),
+        'O' => open(&coordinates)
+        //_ => println!("Bad menu option"), //if none of the above is entered
     };
-    GenInfo2.bottomLine = GenInfo2.topLine + GenInfo2.screenheight;
-    //let mut Split: <> = Vec::new();
-    /*
-    Split[GenInfo2.splitTotal] = split{
-        content: fs::read_to_string(path).expect("bad read"), //contents now equals the content of the file
-        currentLine: (content[0], 0),
-        //fileHistory
-        filename: filename,
-        coordinates: (GenInfo2.splitTotal, GenInfo2.currentTab, GenInfo2.currentDesktop),
-        topLine: 0,
-        bottomLine: GenInfo2.topLine+GenInfo2.screenheight,
-    }
-    */
 }
+fn open(coordinates: &(i32,i32,i32),filename: &String){
+    let file = OpenOptions::new().read(true).write(true).create(true).open(filename); //sets read and write privalages to true, and creates file if its not present
+    //let path = Path::new(&file);
+    let mut text = Rope::from_reader(
+        BufReader::new(File::open(file))
+    ).expect("bad file read into ropes");
+    let start_idx = contents.line_to_char(0);
+    let end_idx = contents.line_to_char(displayHeight);
+    println!("",contents.slice(start_idx..end_idx));
+}
+fn edit(filename: &String){
+    println!("Edit function under contruction");
+}
+//fn as_ref(&self) -> &T;
