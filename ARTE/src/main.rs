b211d83b8339a32c5b7d14ac688da2345c54d533
BoneKing <mahonea@clarkson.edu>
2019-07-24 06:28:19 -0400

    Actually get a menu now

--- ARTE/src/main.rs
+++ ARTE/src/main.rs
@@ -2,13 +2,13 @@
 //last edited on 6/16/19
 
 use std::env;
+use std::io;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::error::Error; 
 //use std::io::self;
-//use std::io::Read;
-//use std::io;
+use std::io::Read;
 use std::fs::OpenOptions;
 use std::fs;
 use std::fs::read_to_string;
@@ -26,29 +26,34 @@ fn main(){
     let mut totalTab: i32 =1; //above but tabs
     let mut totalDesktop: i32 =1; // above but desktops
     let mut coordinates: (i32, i32, i32)=(1,1,1); //coordinates of split tab and desktop in that order to send as arguements 
-    println!("Welcome to ARTE! \n E: Edit file \n O: open file \n S: new split \n T: new tab \n D: new Desktop \n W: Write to file \n Q: quit \n M: return to main menu "); //basic menu
-    mode_Select();
+    //println!("Welcome to ARTE! \n E: Edit file \n O: open file \n S: new split \n T: new tab \n D: new Desktop \n W: Write to file \n Q: quit \n M: return to main menu "); //basic menu
+    println!("Welcome to ARTE! \n");
+    mode_Select(&coordinates, &filename);
 }
-fn mode_Select(){
+fn mode_Select(coordinates: &(i32,i32,i32),filename: &String){
     println!("This doesn't exist yet sorta");
     println!("Mode Select: \n E: Edit file \n O: open file \n S: new split \n T: new tab \n D: new Desktop \n W: Write to file \n Q: quit \n M: return to main menu "); //basic menu
     let mut menuOption = String::new(); 
     io::stdin().read_line(&mut menuOption).expect("input error on menuOption"); //gets menu option selected by user
     match menuOption.as_str(){
-        "E" => edit(&filename),
-        'O' => open(&coordinates)
-        //_ => println!("Bad menu option"), //if none of the above is entered
+        "Edit" => edit(&filename),
+        "Open" => open(&coordinates, &filename),
+        _ => println!("Bad menu option"), //if none of the above is entered
     };
 }
 fn open(coordinates: &(i32,i32,i32),filename: &String){
+    println!("you're inside the open option");
     let file = OpenOptions::new().read(true).write(true).create(true).open(filename); //sets read and write privalages to true, and creates file if its not present
+    let fileContents=fs::read_to_string(filename).expect("bad read of file");
     //let path = Path::new(&file);
-    let mut text = Rope::from_reader(
+    /*let mut contents = Rope::from_reader(
         BufReader::new(File::open(file))
     ).expect("bad file read into ropes");
+    */
+    let mut contents = Rope::from_str(&fileContents);
     let start_idx = contents.line_to_char(0);
     let end_idx = contents.line_to_char(displayHeight);
-    println!("",contents.slice(start_idx..end_idx));
+    println!("{}",contents.slice(start_idx..end_idx));
 }
 fn edit(filename: &String){
     println!("Edit function under contruction");
